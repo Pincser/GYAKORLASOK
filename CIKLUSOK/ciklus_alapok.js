f2817ca40d4769702b5594234427a0a7a5494a7e
Szabó Áron <szabo.aaron@gmail.com>
2025-03-02 11:10:28 +0100

    Add files via upload

---
+++ CIKLUSOK/ciklus_alapok.js
@@ -0,0 +1,32 @@
+<script>
+
+//FOR ciklus
+
+for (let i=0;i<10;i++)
+document.write("<br>i értéke:",i);
+
+document.write("<hr>");
+
+for (let i=0;i<5;i++)
+document.write("<br>i éréke:",i);
+
+document.write("<hr>");
+
+for (let i=0;i<10;i+=2)
+document.write("<br>i értéke:",i);
+
+/* ELÖL TESZTELŐS "while" ciklus
+
+j = 0
+while (j<10);
+{document.write("j értéke:",j); j++;}
+*/
+
+/* HÁTUL TESZTELŐS "do while" ciklus
+
+k = 0
+do {document.write("k értéke:",k); k++;}
+while (k<10);
+*/
+
+</script>
---
+++ CIKLUSOK/ciklus_feladatok_3db.js
@@ -0,0 +1,22 @@
+<script>
+
+// FELADATOK:
+/*	1. 0-25-ig írja ki a számokat
+	2. 0-10-ig írja ki a számokat
+    3. 10-1-ig írja ki a számokat
+*/
+
+for (let i=0;i<26;i++)
+document.write("<br>i értéke:",i);
+
+document.write("<hr>");
+
+for (let j=0;j<12;j+=2)
+document.write("<br>j értéke:",j);
+
+document.write("<hr>");
+
+for (let k=10;k>0;k--)
+document.write("<br>k értéke:",k);
+
+</script>
